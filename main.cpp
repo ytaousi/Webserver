f3976ee4e8825a036eee51b4d9c2ab73874df9a7
ytaousi <yassir.taous@gmail.com>
2022-04-14 11:30:28 +0000

    ...

--- main.cpp
+++ main.cpp
@@ -3,9 +3,65 @@
 #include <vector>
 #include <map>
 #include <algorithm>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/socket.h> // for listenning and binding
+#include <netinet/in.h> // for manipulating addr family structur !!!!??
 
 int main(int ac, char **av)
 {
+    char * response_string = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!"; // char *  vs std::string
+    int server_fd;
+    int new_socket; // socket created when a connection is started between 
+    long value_read;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
 
+
+    address.sin_family = AF_INET; // domain (type of ip address) used to connect to socket
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( 8080 ); // htons convert from numerical value to network representation  from host_byte rep to network_byte rep 
+    
+    //memset c++ ish i need
+    memset(address.sin_zero, '\0', sizeof address.sin_zero);
+    
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        std::cout << "Couldn't Create Socket for Communication" << std::endl;
+        exit(1);
+    }
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) // when the socket is created there is no address assigned to it
+    {
+        std::cout << "Couldn't Bind Socket" << std::endl;
+        exit(1);
+    }
+
+    if (listen(server_fd, 10) < 0)
+    {
+        std::cout << "Server Failed to listen on Port" << std::endl;
+        exit(1);
+    }
+
+    while(1)
+    {
+        printf("\n+++++++ Waiting for new connection ++++++++\n\n");
+        
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) <0 )
+        {
+            std::cout << "Server Failed to Accept Any Connection" << std::endl;
+            exit(1);
+        }
+
+        char buffer[30000] = {0};
+        
+        value_read = read( new_socket , buffer, 30000);
+        
+        printf("%s\n",buffer );
+        write(new_socket , response_string , strlen(response_string));
+        printf("------------------Hello message sent-------------------\n");
+        close(new_socket);
+    }
     return (0);
 }
