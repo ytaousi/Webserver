d9c7406428cdd94b535d2454533411d363d1cbd4
ytaousi <yassir.taous@gmail.com>
2022-04-19 15:20:50 +0000

    ...

--- parseConfigFile.cpp
+++ parseConfigFile.cpp
@@ -1,7 +1,29 @@
 # include <iostream>
 # include <string>
 # include <fstream>
+# include <sstream>
 # include "server.hpp"
+
+std::vector<std::string> getServerBlock(std::istream & configFile)
+{
+    std::vector<std::string> serverBlock;
+    for (std::string line ; std::getline(configFile, line);)
+    {
+        if (line.find("server {", 0) != std::string::npos)
+        {
+            for (; line.find("};", 0) == std::string::npos; std::getline(configFile, line))
+            {
+                serverBlock.push_back(line);
+            }
+        }
+        if (line.find("};", 0) != std::string::npos)
+        {
+            serverBlock.push_back(line);
+            break;
+        }
+    }
+    return serverBlock;
+}
 
 int main(int ac, char **av)
 {
@@ -16,7 +38,7 @@ int main(int ac, char **av)
 
     const std::string configFilePath = av[1];
 
-    std::ifstream configFile;
+    std::ifstream   configFile;
     configFile.open(configFilePath, std::ios::out);
     if (!configFile)
     {
@@ -24,25 +46,29 @@ int main(int ac, char **av)
         exit(1);
     }
 
-    for (std::string line; std::getline(configFile, line);)
-    {
-        std::cout << line << std::endl;
-    }
     printf("++++++++++++++++ extract server block +++++++++++++++++++++++++++++\n");
     // extract server block between brackets from config file.
-    std::vector<std::string> serverBlock; // vector to store server block content between curly brackets.
-    
-    // store server block in serverBlock vector.
-    for (std::string line; std::getline(configFile, line);)
-    {
-        ;      
-    }
-    
-    // print server block vector.
-    for (std::vector<std::string>::iterator it = serverBlock.begin(); it != serverBlock.end(); ++it)
+    //std::vector<server>         serverBlocks; // vector to store server block content between curly brackets.
+    std::vector<std::string>    serverBlock1;
+    std::vector<std::string>    serverBlock2;
+
+
+    serverBlock1 = getServerBlock(configFile);
+    for (std::vector<std::string>::iterator it = serverBlock1.begin(); it != serverBlock1.end(); ++it)
     {
         std::cout << *it << std::endl;
     }
+
+    serverBlock2 = getServerBlock(configFile);
+    for (std::vector<std::string>::iterator it = serverBlock2.begin(); it != serverBlock2.end(); ++it)
+    {
+        std::cout << *it << std::endl;
+    }
+
+
+    // print server blocks c++98 syntax.
+    
+    
     
     printf("++++++++++++++++ EndofServer block +++++++++++++++++++++++++++++\n");
 
