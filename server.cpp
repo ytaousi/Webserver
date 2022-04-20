f90c9a0f630116fdf633dd913b2338211ab484c4
ytaousi <yassir.taous@gmail.com>
2022-04-20 04:41:36 +0000

    ...

---
+++ server.cpp
@@ -0,0 +1,128 @@
+# include "server.hpp"
+
+server::server(const std::vector<std::string> & serverBlock)
+{
+    for (std::vector<std::string>::const_iterator it = serverBlock.begin(); it != serverBlock.end(); ++it)
+    {
+        if (it->find("listen :") != std::string::npos)
+        {
+            if (_port != "")
+                continue;
+            _port = it->substr(it->find("listen :") + 9, it->size());
+        }
+        if (it->find("server_name :") != std::string::npos)
+        {
+            if (_serverName != "")
+            continue;
+            _serverName = it->substr(it->find("server_name :") + 14, it->size());
+        }
+        if (it->find("root :") != std::string::npos)
+        {
+            // if root is not empty continue.
+            if (_root != "")
+                continue;
+            _root = it->substr(it->find("root :") + 7, it->size());
+        }
+        if (it->find("error_page :") != std::string::npos)
+        {
+            std::string errorDirective = it->substr(it->find("error_page :") + 13, it->size());
+
+            _errorPageStatus = errorDirective.substr(0, errorDirective.find(" "));
+            _errorPage = errorDirective.substr(errorDirective.find(" ") + 1, errorDirective.size());
+        }
+        if (it->find("charset :") != std::string::npos)
+        {
+            if (_charSet != "")
+                continue;
+            _charSet = it->substr(it->find("charset :") + 10, it->size());
+        }
+        if (it->find("index :") != std::string::npos)
+        {
+            if (_indexFiles.empty() == false)
+                continue;
+            std::string indexDirective = it->substr(it->find("index :") + 8, it->size());
+            // split index directive by space and fill _indexFiles.
+            std::stringstream ss(indexDirective);
+            std::string       token;
+            while (std::getline(ss, token, ' '))
+            {
+                _indexFiles.push_back(token);
+            }
+        }
+        // if (it->find("location :", 0) != std::string::npos)
+        // {
+        //     
+        // }
+    }
+}
+
+server::~server()
+{
+    // clear server member variables.
+    _port.clear();
+    _serverName.clear();
+    _root.clear();
+    _errorPage.clear();
+    _errorPageStatus.clear();
+    _charSet.clear();
+    _indexFiles.clear();
+    _locations.clear();
+}
+
+// print server configuration.
+void server::printServerConfig() const
+{
+    std::cout << "port: " << _port << std::endl;
+    std::cout << "server_name: " << _serverName << std::endl;
+    std::cout << "root: " << _root << std::endl;
+    std::cout << "error_page: " << _errorPage << std::endl;
+    std::cout << "error_page Status: " << _errorPageStatus << std::endl;
+    std::cout << "charset: " << _charSet << std::endl;
+    std::cout << "indexFiles: " << std::endl;
+    for (std::vector<std::string>::const_iterator it = _indexFiles.begin(); it != _indexFiles.end(); it++)
+    {
+        std::cout << "\t" << *it << std::endl;
+    }
+    std::cout << "locations: " << std::endl;
+    for (std::vector<std::string>::const_iterator it = _locations.begin(); it != _locations.end(); ++it)
+    {
+        std::cout << *it << std::endl;
+    }
+}
+
+
+// getters
+std::string server::getPort() const
+{
+    return _port;
+}
+
+std::string server::getServerName() const
+{
+    return _serverName;
+}
+
+std::string server::getRoot() const
+{
+    return _root;
+}
+
+std::string server::getErrorPage() const
+{
+    return _errorPage;
+}
+
+std::string server::getCharSet() const
+{
+    return _charSet;
+}
+
+std::vector<std::string> server::getIndexFiles() const
+{
+    return _indexFiles;
+}
+
+std::vector<std::string> server::getLocations() const
+{
+    return _locations;
+}
--- server.hpp
+++ server.hpp
@@ -21,120 +21,20 @@ class server
 
 
     public:
-        server(const std::vector<std::string> & serverBlock)
-        {
-            for (std::vector<std::string>::const_iterator it = serverBlock.begin(); it != serverBlock.end(); ++it)
-            {
-                if (it->find("listen :") != std::string::npos)
-                {
-                    if (_port != "")
-                        continue;
-                    _port = it->substr(it->find("listen :") + 9, it->size());
-                }
-                if (it->find("server_name :") != std::string::npos)
-                {
-                    if (_serverName != "")
-                        continue;
-                    _serverName = it->substr(it->find("server_name :") + 14, it->size());
-                }
-                if (it->find("root :") != std::string::npos)
-                {
-                    // if root is not empty continue.
-                    if (_root != "")
-                        continue;
-                    _root = it->substr(it->find("root :") + 7, it->size());
-                }
-                if (it->find("error_page :") != std::string::npos)
-                {
-                    std::string errorDirective = it->substr(it->find("error_page :") + 13, it->size());
-
-                    _errorPageStatus = errorDirective.substr(0, errorDirective.find(" "));
-                    _errorPage = errorDirective.substr(errorDirective.find(" ") + 1, errorDirective.size());
-                }
-                if (it->find("charset :") != std::string::npos)
-                {
-                    if (_charSet != "")
-                        continue;
-                    _charSet = it->substr(it->find("charset :") + 10, it->size());
-                }
-                if (it->find("index :") != std::string::npos)
-                {
-                    if (_indexFiles.empty() == false)
-                        continue;
-                    std::string indexDirective = it->substr(it->find("index :") + 8, it->size());
-                    // split index directive by space and fill _indexFiles.
-                    std::stringstream ss(indexDirective);
-                    std::string       token;
-                    while (std::getline(ss, token, ' '))
-                    {
-                        _indexFiles.push_back(token);
-                    }
-                }
-                // if (it->find("location :", 0) != std::string::npos)
-                // {
-                //     
-                // }
-            }
-        }
-        ~server()
-        {
-            std::cout << "server destructor" << std::endl;
-        }
-        void printServerConfig() const
-        {
-            std::cout << "port: " << _port << std::endl;
-            std::cout << "server_name: " << _serverName << std::endl;
-            std::cout << "root: " << _root << std::endl;
-            std::cout << "error_page: " << _errorPage << std::endl;
-            std::cout << "error_page Status: " << _errorPageStatus << std::endl;
-            std::cout << "charset: " << _charSet << std::endl;
-            std::cout << "indexFiles: " << std::endl;
-            for (std::vector<std::string>::const_iterator it = _indexFiles.begin(); it != _indexFiles.end(); it++)
-            {
-                std::cout << "\t" << *it << std::endl;
-            }
-            std::cout << "locations: " << std::endl;
-            for (std::vector<std::string>::const_iterator it = _locations.begin(); it != _locations.end(); ++it)
-            {
-                std::cout << *it << std::endl;
-            }
-        }
+        server(const std::vector<std::string> & serverBlock);
+        ~server();
+        
+        // print server configuration.
+        void printServerConfig() const;
         
         // getters.
-        std::string getPort() const
-        {
-            return _port;
-        }
-
-        std::string getServerName() const
-        {
-            return _serverName;
-        }
-
-        std::string getRoot() const
-        {
-            return _root;
-        }
-
-        std::string getErrorPage() const
-        {
-            return _errorPage;
-        }
-
-        std::string getCharSet() const
-        {
-            return _charSet;
-        }
-
-        std::vector<std::string> getIndexFiles() const
-        {
-            return _indexFiles;
-        }
-
-        std::vector<std::string> getLocations() const
-        {
-            return _locations;
-        }
+        std::string getPort() const;
+        std::string getServerName() const;
+        std::string getRoot() const;
+        std::string getErrorPage() const;
+        std::string getCharSet() const;
+        std::vector<std::string> getIndexFiles() const;
+        std::vector<std::string> getLocations() const;
 
 
 
