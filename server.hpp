f5b3d527991c853a79a05d8e6fd2b6434213a460
ytaousi <yassir.taous@gmail.com>
2022-04-20 04:22:34 +0000

    config file twicked to easee parsing noob

--- server.hpp
+++ server.hpp
@@ -5,6 +5,7 @@
 # include <string>
 # include <map>
 # include <iostream>
+# include <sstream>
 
 class server
 {
@@ -24,43 +25,52 @@ class server
         {
             for (std::vector<std::string>::const_iterator it = serverBlock.begin(); it != serverBlock.end(); ++it)
             {
-                if (it->find("listen") != std::string::npos)
+                if (it->find("listen :") != std::string::npos)
                 {
                     if (_port != "")
                         continue;
-                    _port = it->substr(it->find("listen") + 7, it->size());
+                    _port = it->substr(it->find("listen :") + 9, it->size());
                 }
-                if (it->find("server_name") != std::string::npos)
+                if (it->find("server_name :") != std::string::npos)
                 {
                     if (_serverName != "")
                         continue;
-                    _serverName = it->substr(it->find("server_name") + 12, it->size());
+                    _serverName = it->substr(it->find("server_name :") + 14, it->size());
                 }
-                if (it->find("root") != std::string::npos)
+                if (it->find("root :") != std::string::npos)
                 {
                     // if root is not empty continue.
                     if (_root != "")
                         continue;
-                    _root = it->substr(it->find("root") + 5, it->size());
+                    _root = it->substr(it->find("root :") + 7, it->size());
                 }
-                if (it->find("error_page") != std::string::npos)
+                if (it->find("error_page :") != std::string::npos)
                 {
-                    std::string errorDirective = it->substr(it->find("error_page") + 11, it->size());
+                    std::string errorDirective = it->substr(it->find("error_page :") + 13, it->size());
 
                     _errorPageStatus = errorDirective.substr(0, errorDirective.find(" "));
                     _errorPage = errorDirective.substr(errorDirective.find(" ") + 1, errorDirective.size());
                 }
-                if (it->find("charset") != std::string::npos)
+                if (it->find("charset :") != std::string::npos)
                 {
                     if (_charSet != "")
                         continue;
-                    _charSet = it->substr(it->find("charset") + 8, it->size());
+                    _charSet = it->substr(it->find("charset :") + 10, it->size());
                 }
-                // if (it->find("indexFiles", 0) != std::string::npos)
-                // {
-                //     ;
-                // }
-                // if (it->find("location", 0) != std::string::npos)
+                if (it->find("index :") != std::string::npos)
+                {
+                    if (_indexFiles.empty() == false)
+                        continue;
+                    std::string indexDirective = it->substr(it->find("index :") + 8, it->size());
+                    // split index directive by space and fill _indexFiles.
+                    std::stringstream ss(indexDirective);
+                    std::string       token;
+                    while (std::getline(ss, token, ' '))
+                    {
+                        _indexFiles.push_back(token);
+                    }
+                }
+                // if (it->find("location :", 0) != std::string::npos)
                 // {
                 //     
                 // }
@@ -72,18 +82,18 @@ class server
         }
         void printServerConfig() const
         {
-            std::cout << "Port: " << _port << std::endl;
-            std::cout << "Server Name: " << _serverName << std::endl;
-            std::cout << "Root: " << _root << std::endl;
-            std::cout << "Error Page: " << _errorPage << std::endl;
-            std::cout << "Error Page Status: " << _errorPageStatus << std::endl;
-            std::cout << "Character Set: " << _charSet << std::endl;
-            std::cout << "Index Files: " << std::endl;
-            for (std::vector<std::string>::const_iterator it = _indexFiles.begin(); it != _indexFiles.end(); ++it)
+            std::cout << "port: " << _port << std::endl;
+            std::cout << "server_name: " << _serverName << std::endl;
+            std::cout << "root: " << _root << std::endl;
+            std::cout << "error_page: " << _errorPage << std::endl;
+            std::cout << "error_page Status: " << _errorPageStatus << std::endl;
+            std::cout << "charset: " << _charSet << std::endl;
+            std::cout << "indexFiles: " << std::endl;
+            for (std::vector<std::string>::const_iterator it = _indexFiles.begin(); it != _indexFiles.end(); it++)
             {
-                std::cout << *it << std::endl;
+                std::cout << "\t" << *it << std::endl;
             }
-            std::cout << "Locations: " << std::endl;
+            std::cout << "locations: " << std::endl;
             for (std::vector<std::string>::const_iterator it = _locations.begin(); it != _locations.end(); ++it)
             {
                 std::cout << *it << std::endl;
